5eb489546ac5034eb08c474a468ba9df81127526
FrankHuarcaya <u201624288@upc.edu.pe>
2022-09-28 13:34:32 -0500

    feat:Graphs
    graphics were added to the project

--- src/app/parking/pages/parking-graphs/parking-graphs.component.ts
+++ src/app/parking/pages/parking-graphs/parking-graphs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApexChart, ApexNonAxisChartSeries, ApexTitleSubtitle} from "ng-apexcharts";
 
 @Component({
   selector: 'app-parking-graphs',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parking-graphs.component.css']
 })
 export class ParkingGraphsComponent implements OnInit {
+
+
+  series: ApexNonAxisChartSeries=[5,4,5,6];
+
+
+  labels=["santa anita","jockey","Mall de sur","real plaza"];
+
+  charDetails:ApexChart={
+    type:'pie'
+
+  }
+  chartTitle: ApexTitleSubtitle={
+    text:'Leading companies',
+    align:'center'
+  }
 
   constructor() { }
 
