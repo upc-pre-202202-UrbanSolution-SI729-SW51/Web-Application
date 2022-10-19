f3fdcdb61523287e0673cc31470ad5958ca6bab1
FrankHuarcaya <u201624288@upc.edu.pe>
2022-10-19 18:43:47 -0500

    feat(new changes) Added path reservation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {ParkingLotsComponent} from "./parking/pages/parking-lots/parking-lots.component";
 import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 import {ReservationHistoryComponent} from "./parking/pages/reservation-history/reservation-history.component";
+import {ReservationComponent} from "./parking/pages/reservation/reservation.component";
 
 
 const routes: Routes = [
   { path: 'parking-lots', component: ParkingLotsComponent},
   { path: 'reservation-history', component: ReservationHistoryComponent},
   { path: '', redirectTo: 'parking-lots', pathMatch: 'full'},
+  {path:'reservation', component:ReservationComponent},
   { path: '**', component: PageNotFoundComponent }
 ];
 
