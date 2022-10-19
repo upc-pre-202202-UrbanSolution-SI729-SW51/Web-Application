b08da7d557917fabca7294e35825b8ad5f62f0ff
Asura <u202020239@upc.edu.pe>
2022-10-19 00:02:59 -0500

    feat(new changes) Added new components

---
+++ src/app/parking/pages/reservation-history/reservation-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-history',
+  templateUrl: './reservation-history.component.html',
+  styleUrls: ['./reservation-history.component.css']
+})
+export class ReservationHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
