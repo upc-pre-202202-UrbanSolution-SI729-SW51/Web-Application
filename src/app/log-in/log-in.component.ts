0eed18002d13d5fb67b1bdb4165bb8f3a683ad0d
JoseR200 <joserodrigolopez@icloud.com>
2022-11-07 18:35:37 -0500

    feat: Fixing Security Branch v2

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-log-in',
-  templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.css']
+  templateUrl: './log-in.component.html'
 })
 export class LogInComponent implements OnInit {
 
